c71d9464bebb295a5ad83598bc78e366896a5372
Hack Reactor Student <student@hackreactor.com>
2017-06-10 14:56:38 -0700

    Completed helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var fetch = require('node-fetch');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,12 +26,6 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-// exports.readListOfUrls = function(data) {
-//   return data.toString().split('\n');
-// };
-
-
-
 exports.readListOfUrls = function(callback) {
   fs.readFile(this.paths.list, 'utf8', function(error, data) {
     callback(data.split('\n'));   
@@ -44,10 +39,25 @@ exports.isUrlInList = function(url, callback) {
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.writeFile(this.paths.list, url, function(error) {
+    return callback();
+  });
 };
 
 exports.isUrlArchived = function(url, callback) { 
+  fs.readdir(this.paths.archivedSites, function(error, files) {
+    return callback(files.includes(url));
+  });
 };
 
 exports.downloadUrls = function(urls) {
+  var path;
+  urls.forEach(url => {
+    path = this.paths.archivedSites + '/' + url;
+    fetch('https://' + url).then(function(res) {
+      return res.text();
+    }).then(function(body) {
+      fs.writeFile(path, body);      
+    });
+  });
 };
--- test/test.js
+++ test/test.js
@@ -112,9 +112,10 @@ describe('archive helpers', function() {
     it('should add a url to the list', function (done) {
       var urlArray = ['example1.com', 'example2.com\n'];
       fs.writeFileSync(archive.paths.list, urlArray.join('\n'));
-
+      // console.log('ADDURLTOLIST ---->, ', urlArray);
       archive.addUrlToList('someurl.com', function () {
         archive.isUrlInList('someurl.com', function (exists) {
+          // console.log('URLARRAY', urlArry);
           expect(exists).to.be.true;
           done();
         });
